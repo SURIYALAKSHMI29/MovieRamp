9f89c3b48bdb4d26ef41007b0046070669c7eb0e
SURIYALAKSHMI29 <suriyalakshmi413@gmail.com>
2025-03-24 00:23:55 +0530

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        light: {
+          100: "#cecefb",
+          200: "#a8b5db",
+        },
+        gray: {
+          100: "#9ca4ab",
+        },
+        dark: {
+          100: "#0f0d23",
+        },
+      },
+      fontFamily: {
+        dmSans: ["DM Sans", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/hero-bg.png')",
+      },
+      screens: {
+        xs: "480px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
